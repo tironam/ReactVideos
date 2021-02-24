3e21b4e44e29585a2cc9f1127d1ee164387dfeb0
Michael <tironamichael@gmail.com>
2021-02-24 10:56:49 -0800

    Finished adding project code

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem />;
+        return (
+            <VideoItem 
+                key={video.id.videoId} 
+                onVideoSelect={onVideoSelect} 
+                video={video} 
+            />
+        );
     })
 
-    return <div>{renederedList}</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
 export default VideoList;
