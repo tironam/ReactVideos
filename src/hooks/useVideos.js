28dc9a4dd1cc565657a42a7e78d7ebef213ae6c4
Michael <tironamichael@gmail.com>
2021-03-15 12:29:05 -0700

    Changed onTermSubmit to search

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import youtube from '../apis/youtube';
 
-const useVideos = () => {
+const useVideos = (defaultSearchTerm) => {
     // Used to manage a list of videos
     const [videos, setVideos] = useState([]);
 
     // Included empty array because we want this to run only one time
     // This is roughly equivalent to componentDidMount
     useEffect(() => {
-        onTermSubmit('buildings');
-    }, [])
+        search(defaultSearchTerm);
+    }, []);
 
     const onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -20,6 +20,9 @@ const useVideos = () => {
 
         setVideos(response.data.items);
     };
+
+    // React convention is to return an array
+    return [videos, search];
 };
 
 export default useVideos;
