61adbff607d882c586f8f65511e98b8875f5d373
Michael <tironamichael@gmail.com>
2021-03-15 12:33:14 -0700

    Added usVideos hook to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React,  { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
-import VideoDetail from './VideoDetail'
+import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
     // Used to manage the selected video
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('Disneyland');
  
-// setSelectedVideo(response.data.items[0]);
+    useEffect(() => {
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
     return (
             <div className="ui container">
-                <SearchBar onFormSubmit={onTermSubmit} />
+                <SearchBar onFormSubmit={search} />
                 <div className="ui grid">
                     <div className="ui row">
                     <div className="eleven wide column">
